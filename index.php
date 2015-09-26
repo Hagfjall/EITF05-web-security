a94669edbb9e3120ae848fc6be63191d2d5910b1
Axel Smeets <dat12asm@student.lu.se>
2015-09-26 22:19:16 +0200

    update index.php to use menu.php check auth state

--- index.php
+++ index.php
@@ -9,19 +9,28 @@ require_once "database.inc.php";
 </head>
 
 <body>
-<form name="login_form" method="post" action="shop.php">
-    Email:<br>
-    <input type="text" name="email" class="input">
-    <br>
-    Password:<br>
-    <input type="password" name="password" class="input">
-    <br>
-    <input type="submit" value="Login">
-</form>
-<a href="signup_form.html">Sign up</a>
+    <?php require 'menu.php'; ?>
+<?php
+    if (isset($_SESSION["auth"])) {
+        print "<h3>welcome back, ".$_SESSION["email"]."!</h3>";
+    } else {
+    ?>
+    <form name="login_form" method="post" action="login.php">
+        Email:<br>
+        <input type="text" name="email" class="big">
+        <br>
+        Password:<br>
+        <input type="password" name="password" class="big">
+        <br>
+        <input type="submit" value="Login" class="big">
+    </form>
+    <?php
+    }
+?>
 <br>
 All the users that bought amazing expl0itz from us!<br>
 <div id="tableTag"></div>
 <script src="js/index.js"></script>
 </body>
+
 </html>
