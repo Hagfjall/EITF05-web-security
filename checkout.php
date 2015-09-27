5faa9a6cd5b1b3e4b34a731a7b86bbd867fbc61c
Axel Smeets <dat12asm@student.lu.se>
2015-09-27 15:54:12 +0200

    update checkout.php to generate csrf token alternative

--- checkout.php
+++ checkout.php
@@ -1,3 +1,8 @@
+<?php
+	require_once "database.inc.php";
+	require_once 'csrf.php';
+?>
+
 <!DOCTYPE html>
 <html>
     <head>
@@ -5,29 +10,33 @@
     </head>
 
     <body>
-		<?php require 'menu.php'; ?>
+    	<?php require 'menu.php'; ?>
 
 		<?php
-		require_once "database.inc.php";
-
-		$count = 0;
-		$items = $db->getAllItemsInShop();
-		for ($i = 0; $i < count($items); $i++) {
-			if (isset($_SESSION['shopping_cart'][$i])) {
-		    	$count += $_SESSION['shopping_cart'][$i];
+			$count = 0;
+			$items = $db->getAllItemsInShop();
+			for ($i = 0; $i < count($items); $i++) {
+				if (isset($_SESSION['shopping_cart'][$i])) {
+			    	$count += $_SESSION['shopping_cart'][$i];
+				}
 			}
-		}
-		if ($count == 0){
-		    print "You have no exploits in your shopping cart";
-		    print '<a href="shop.php">Go back</a>';
-		    die();
-		} else {
-		    print "Transfer ";
-		    print $count;
-		    print " bitcoins to bitcoin account 999999999 to purchase";
-		}
+			if ($count == 0){
+			    print "You have no exploits in your shopping cart";
+			    print '<a href="shop.php">Go back</a>';
+			    die();
+			} else {
+			    print "Transfer ";
+			    print $count;
+			    print " bitcoins to bitcoin account 999999999 to purchase";
+			}
 		?>
 		<br>
-		<a href="receipt.php">Transfer money</a>
+		<form name="checkout_form" method="POST" action="receipt.php">
+			<?php
+				echo '<input type="hidden" name="csrf_token" value="'.init_csrf_token("checkout_form").'">';
+			?>
+			<input type="submit" value="transfer money (safe)">
+		</form>
+		<a href="receipt_unsafe.php">Transfer money (unsafe)</a>
 	</body>
 </html>
